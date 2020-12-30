0a570f7c3aafa1d4a8684cfbd36ffdfca17c5262
duckgus <duckdi97@gmail.com>
2020-12-30 17:52:02 +0900

    spring2-2 db연결&테스트

--- hello-spring/src/main/java/hellospring/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hellospring/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hellospring.hellospring;
 
+import hellospring.hellospring.repository.JdbcMemberRepository;
 import hellospring.hellospring.repository.MemberRepository;
 import hellospring.hellospring.repository.MemoryMemberRepository;
 import hellospring.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +24,7 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
