82cb2cda591483e36e472889859f0a4ecb07b481
duckgus <duckdi97@gmail.com>
2021-01-05 15:58:02 +0900

    3 fail

---
+++ hello-spring3/src/main/java/hello/hellospring3/controller/MemberController.java
@@ -0,0 +1,45 @@
+package hello.hellospring3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hello.hellospring3.domain.Member;
+import hello.hellospring3.service.MemberService;
+
+@Controller
+public class MemberController {
+
+	private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMember";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.fineMembers();
+        model.addAttribute("members", members);
+        return "members/membersList";
+    }
+}
