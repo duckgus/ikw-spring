8b524598865979af993a7b5a008da581b400cfac
duckgus <duckdi97@gmail.com>
2021-01-07 11:24:57 +0900

    maven

---
+++ hello-Maven/src/test/java/hello/helloMaven/HelloMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.helloMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
